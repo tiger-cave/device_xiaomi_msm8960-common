637e764d29d75375b04ed4cd1ed3c912c0e1c87b
GuaiYiHu <guaiyihu@foxmail.com>
2017-08-20 14:03:29 +0800

    msm8960: init: Explicitly include unistd.h
    Change-Id: I3c37ed2b6e830b40acdc7de0c908aef7b64ab942

--- init/init_msm8960.cpp
+++ init/init_msm8960.cpp
@@ -32,6 +32,7 @@
 #include <fcntl.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <unistd.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
