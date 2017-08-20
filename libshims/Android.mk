5a3abd3f1e546aa635a5b441dcd44542732812a3
GuaiYiHu <guaiyihu@foxmail.com>
2017-08-20 16:36:37 +0800

    msm8960: update libshim
    Change-Id: Iccb6eda58aa1efe198c8ef8fcdcfa91f6517c853

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -17,6 +17,7 @@ LOCAL_SRC_FILES := \
 LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware libui libgui libbinder libutils libsync
 LOCAL_MODULE := libshim_camera
 LOCAL_C_INCLUDES += external/safe-iop/include
+LOCAL_C_INCLUDES += system/core/libutils
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_MODULE_TAGS := optional
