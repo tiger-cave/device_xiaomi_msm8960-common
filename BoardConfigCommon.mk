26a490c96a9ec89e48eebee58e0deae4cb282d63
Anton Hansson <hansson@google.com>
2019-02-21 15:04:20 +0000

    msm8960-common: Set cpu "runtime variant" to krait
    This makes bionic and various art tools that compile/runs cpu-specific
    code choose the krait variant at runtime. It does not affect
    build-time compilation.
    
    Bug: 120773446
    Test: bionic benchmarks with and without this change
    Change-Id: I670c8acd094090135515688c5f057c2cc08a083f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,6 +22,7 @@ TARGET_CPU_ABI             := armeabi-v7a
 TARGET_CPU_ABI2            := armeabi
 TARGET_CPU_SMP             := true
 TARGET_CPU_VARIANT         := generic
+TARGET_CPU_VARIANT_RUNTIME := krait
 TARGET_ARCH                := arm
 TARGET_ARCH_VARIANT        := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
