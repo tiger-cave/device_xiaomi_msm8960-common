4ab2dad2a2ca64050e3ed2bf07299d583c46b22f
Roshan Pius <rpius@google.com>
2019-03-06 11:23:18 -0800

    msm8960-common: Turn off SoftAP MAC randomization
    Device does not seem to support MAC reconfiguration.
    
    Bug: 127559063
    Test: Manual verification of toggling softap on/off.
    Change-Id: I9ae25b4d14a0c207188fae2c52ac7791b2061a40

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -217,6 +217,7 @@ TARGET_USES_QCOM_WCNSS_QMI       := true
 TARGET_WCNSS_MAC_PREFIX          := e8bba8
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 
 -include vendor/xiaomi/msm8960-common/BoardConfigVendor.mk
