ce193112584dd4568b67cc2e1a4ae91998b987f6
Jimmy Chen <jimmycmchen@google.com>
2019-10-03 11:24:23 +0800

    msm8960-common: Move supplicant service init to wpa_supplicant
    Bug: 138790309
    Test: boot and wifi function is ok
    Change-Id: I35cf3e03566443e20799623045eacd423fd7f5c4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -218,6 +218,7 @@ TARGET_WCNSS_MAC_PREFIX          := e8bba8
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 
 -include vendor/xiaomi/msm8960-common/BoardConfigVendor.mk
