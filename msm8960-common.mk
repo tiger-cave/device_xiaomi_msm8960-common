235feef5d9b88b99c6fa2bb6aa917433b93854fa
Michael Bestas <mkbestas@lineageos.org>
2019-10-05 22:50:26 +0300

    msm8960-common: Update to power HAL 1.2
    Change-Id: Ic8c5f7e9c77a95befff5661e1634683ec6c25ac4

--- msm8960-common.mk
+++ msm8960-common.mk
@@ -218,7 +218,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Ramdisk
 PRODUCT_PACKAGES += \
