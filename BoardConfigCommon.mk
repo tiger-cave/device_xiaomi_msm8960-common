73fa2817a03bba300c556726d1df82d426e9f8ab
Wang Han <416810799@qq.com>
2019-10-11 15:30:06 +0800

    msm8960-common: Sync dexpreopt options from nile-common
    Change-Id: I287db63b8ddb02e8e9e1f18858362ef5af3a5805

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -92,12 +92,10 @@ TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY := camera_parameters_xiaomi_msm8960
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
+    WITH_DEXPREOPT ?= true
   endif
 endif
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # Display
 BOARD_USES_LEGACY_MMAP          := true
