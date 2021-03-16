fadc9c2fc57e171def699fd28d4126ccb089fc33
GuaiYiHu <guaiyihu@foxmail.com>
2021-03-16 09:55:53 +0800

    msm8960-common: Drop mokee stuffs
    Change-Id: I2faabd53bc226c8d0cc6cc372010754ad0ccb2e8

--- msm8960-common.mk
+++ msm8960-common.mk
@@ -23,8 +23,7 @@ LOCAL_PATH := device/xiaomi/msm8960-common
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-mokee
+    $(LOCAL_PATH)/overlay
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
@@ -275,10 +274,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/_hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.mokee.trust@1.0-service
-
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
@@ -305,10 +300,6 @@ PRODUCT_PACKAGES += \
     wificond \
     wpa_supplicant \
     wpa_supplicant.conf
-
-# XiaomiParts
-PRODUCT_PACKAGES += \
-    XiaomiParts
 
 # call the proprietary setup
 $(call inherit-product, vendor/xiaomi/msm8960-common/msm8960-common-vendor.mk)
